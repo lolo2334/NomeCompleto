b980eade7a530afb7d72a3f039550983e1978ffb
Eloa <antuneseloa23@gmail.com>
2025-09-25 20:05:33 -0300

    atividade concluida

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿
+string sobrenome, nome;
+Console.WriteLine("Digite seu sobrenome:");
+sobrenome = Console.ReadLine()!;
+Console.WriteLine("Digite seu nome: ");
+nome = Console.ReadLine()!;
+Console.WriteLine($"{nome} {sobrenome}");
+Console.WriteLine($"{sobrenome.ToUpper()}, {nome}");
